c19be78e8c1b5f401d3e7c9cdb9a98cace1aff80
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-11-14 20:57:12 +0530

    scm

--- src/main/java/com/hcm/scm20/config/SecurityConfig.java
+++ src/main/java/com/hcm/scm20/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.hcm.scm20.config;
 
+import java.io.IOException;
 import java.security.AuthProvider;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,11 +9,19 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 import com.hcm.scm20.services.impl.SecurityCustomUserDetailService;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @Configuration
 public class SecurityConfig {
@@ -65,7 +74,37 @@ public class SecurityConfig {
          });
            //form default login
            //agar hame kuch bhee change karna hogo to yaha ayenge form login se related
-         httpSecurity.formLogin(Customizer.withDefaults());
+         httpSecurity.formLogin(formLogin ->{
+
+          formLogin.loginPage("/login");
+          formLogin.loginProcessingUrl("/authenticate");
+          formLogin.successForwardUrl("/user/dashboard");
+          formLogin.failureForwardUrl("/login?error=true");
+          formLogin.usernameParameter("email");
+          formLogin.passwordParameter("password");
+          // formLogin.failureHandler(new AuthenticationFailureHandler() {
+
+          //   @Override
+          //   public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
+          //       AuthenticationException exception) throws IOException, ServletException {
+          //     // TODO Auto-generated method stub
+          //     throw new UnsupportedOperationException("Unimplemented method 'onAuthenticationFailure'");
+          //   }
+            
+          // });
+
+          // formLogin.successHandler(new AuthenticationSuccessHandler() {
+
+          //   @Override
+          //   public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+          //       Authentication authentication) throws IOException, ServletException {
+          //     // TODO Auto-generated method stub
+          //     throw new UnsupportedOperationException("Unimplemented method 'onAuthenticationSuccess'");
+          //   }
+            
+          // });
+          
+         });
         return httpSecurity.build();
 
        }
