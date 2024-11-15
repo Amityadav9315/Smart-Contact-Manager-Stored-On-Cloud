b4e9d8f44369d35810ac55f908019975c90dde67
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-11-15 15:11:27 +0530

    scm

--- src/main/java/com/hcm/scm20/controllers/UserController.java
+++ src/main/java/com/hcm/scm20/controllers/UserController.java
@@ -1,6 +1,7 @@
 package com.hcm.scm20.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -12,14 +13,14 @@ public class UserController {
 
 
     //user dashboard page
-    @RequestMapping(value = "/dashboard",method = RequestMethod.GET)
+    @GetMapping("/dashboard")
     public String userDashbaord() {
         System.out.println("User dashboard");
         return "user/dashboard";
     }
 
     //user profile page
-    @RequestMapping(value = "/profile",method = RequestMethod.GET)
+    @GetMapping("/profile")
     public String userProfile() {
         System.out.println("User profile");
         return "user/profile";
